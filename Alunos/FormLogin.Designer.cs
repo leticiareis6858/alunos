fdc02693118fc5e3773e7535e97016338bc7e834
Ana Leticia Vieira <leticiareisvr2@gmail.com>
2024-08-20 17:51:36 -0300

    feat: adiciona e configura o formulario de login

---
+++ Alunos/FormLogin.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Alunos
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.link_cadastrar = new System.Windows.Forms.LinkLabel();
+            this.link_mudar_senha = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_entrar = new System.Windows.Forms.Button();
+            this.txt_senha = new System.Windows.Forms.TextBox();
+            this.txt_email = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(257, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(118, 158);
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // link_cadastrar
+            // 
+            this.link_cadastrar.AutoSize = true;
+            this.link_cadastrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.link_cadastrar.Location = new System.Drawing.Point(110, 234);
+            this.link_cadastrar.Name = "link_cadastrar";
+            this.link_cadastrar.Size = new System.Drawing.Size(151, 15);
+            this.link_cadastrar.TabIndex = 14;
+            this.link_cadastrar.TabStop = true;
+            this.link_cadastrar.Text = "Novo usuário? Clique aqui";
+            this.link_cadastrar.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.link_cadastrar_LinkClicked);
+            // 
+            // link_mudar_senha
+            // 
+            this.link_mudar_senha.AutoSize = true;
+            this.link_mudar_senha.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.link_mudar_senha.Location = new System.Drawing.Point(27, 153);
+            this.link_mudar_senha.Name = "link_mudar_senha";
+            this.link_mudar_senha.Size = new System.Drawing.Size(181, 15);
+            this.link_mudar_senha.TabIndex = 13;
+            this.link_mudar_senha.TabStop = true;
+            this.link_mudar_senha.Text = "Esqueceu a senha? Clique aqui";
+            this.link_mudar_senha.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.link_mudar_senha_LinkClicked);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 20);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Senha:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 20);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "E-mail:";
+            // 
+            // btn_entrar
+            // 
+            this.btn_entrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_entrar.Location = new System.Drawing.Point(134, 196);
+            this.btn_entrar.Name = "btn_entrar";
+            this.btn_entrar.Size = new System.Drawing.Size(89, 35);
+            this.btn_entrar.TabIndex = 10;
+            this.btn_entrar.Text = "Entrar";
+            this.btn_entrar.UseVisualStyleBackColor = true;
+            this.btn_entrar.Click += new System.EventHandler(this.btn_entrar_Click);
+            // 
+            // txt_senha
+            // 
+            this.txt_senha.Location = new System.Drawing.Point(30, 130);
+            this.txt_senha.Name = "txt_senha";
+            this.txt_senha.PasswordChar = '*';
+            this.txt_senha.Size = new System.Drawing.Size(176, 20);
+            this.txt_senha.TabIndex = 9;
+            // 
+            // txt_email
+            // 
+            this.txt_email.Location = new System.Drawing.Point(30, 60);
+            this.txt_email.Name = "txt_email";
+            this.txt_email.Size = new System.Drawing.Size(176, 20);
+            this.txt_email.TabIndex = 8;
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 268);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.link_cadastrar);
+            this.Controls.Add(this.link_mudar_senha);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_entrar);
+            this.Controls.Add(this.txt_senha);
+            this.Controls.Add(this.txt_email);
+            this.Name = "FormLogin";
+            this.Text = "FormLogin";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.LinkLabel link_cadastrar;
+        private System.Windows.Forms.LinkLabel link_mudar_senha;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_entrar;
+        private System.Windows.Forms.TextBox txt_senha;
+        private System.Windows.Forms.TextBox txt_email;
+    }
+}
---
+++ Alunos/FormLogin.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Alunos
+{
+    public partial class FormLogin : Form
+    {
+        private Database db;
+        public FormLogin()
+        {
+            InitializeComponent();
+            db = new Database();
+            db.CreateTables();
+        }
+
+        private void btn_entrar_Click(object sender, EventArgs e)
+        {
+            if (txt_email.Text == null)
+            {
+                MessageBox.Show("Informe um email");
+            }
+            else if (txt_senha.Text == null)
+            {
+                MessageBox.Show("Informe uma senha");
+            }
+
+            string email = txt_email.Text.Trim();
+            string senha = txt_senha.Text.Trim();
+
+            bool usuarioValido = db.VerificarLogin(email, senha);
+
+            if (usuarioValido)
+            {
+                FormTelaInicial telaInicial = new FormTelaInicial();
+                this.Hide();
+                telaInicial.ShowDialog();
+                this.Close();
+
+            }
+            else
+            {
+                MessageBox.Show("Email ou senha inválidos!");
+            }
+        }
+
+        private void link_mudar_senha_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            FormRecuperarSenha recuperarSenha = new FormRecuperarSenha();
+            this.Hide();
+            recuperarSenha.ShowDialog();
+            this.Close();
+        }
+
+        private void link_cadastrar_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            FormTelaCadastroUsuario cadastroUsuario = new FormTelaCadastroUsuario();
+            this.Hide();
+            cadastroUsuario.ShowDialog();
+            this.Close();
+        }
+    }
+}
