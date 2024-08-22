2d5e17b43ac48e61b60c81e471f2528c33c253c5
Ana Leticia Vieira <leticiareisvr2@gmail.com>
2024-08-22 20:43:11 -0300

    feat: cria e configura o formulario para excluir alunos

---
+++ Alunos/FormExcluirAluno.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Alunos
+{
+    partial class FormExcluirAluno
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView_alunos = new System.Windows.Forms.DataGridView();
+            this.txt_id = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_excluir = new System.Windows.Forms.Button();
+            this.btn_voltar = new System.Windows.Forms.Button();
+            this.btn_limpar = new System.Windows.Forms.Button();
+            this.btn_sair = new System.Windows.Forms.Button();
+            this.txt_nome = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_alunos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView_alunos
+            // 
+            this.dataGridView_alunos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_alunos.Location = new System.Drawing.Point(55, 156);
+            this.dataGridView_alunos.Name = "dataGridView_alunos";
+            this.dataGridView_alunos.Size = new System.Drawing.Size(682, 250);
+            this.dataGridView_alunos.TabIndex = 0;
+            // 
+            // txt_id
+            // 
+            this.txt_id.Location = new System.Drawing.Point(63, 25);
+            this.txt_id.Multiline = true;
+            this.txt_id.Name = "txt_id";
+            this.txt_id.Size = new System.Drawing.Size(100, 100);
+            this.txt_id.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ID do Aluno:";
+            // 
+            // btn_excluir
+            // 
+            this.btn_excluir.Location = new System.Drawing.Point(169, 103);
+            this.btn_excluir.Name = "btn_excluir";
+            this.btn_excluir.Size = new System.Drawing.Size(181, 34);
+            this.btn_excluir.TabIndex = 3;
+            this.btn_excluir.Text = "Gravar";
+            this.btn_excluir.UseVisualStyleBackColor = true;
+            this.btn_excluir.Click += new System.EventHandler(this.btn_excluir_Click);
+            // 
+            // btn_voltar
+            // 
+            this.btn_voltar.Location = new System.Drawing.Point(646, 19);
+            this.btn_voltar.Name = "btn_voltar";
+            this.btn_voltar.Size = new System.Drawing.Size(81, 30);
+            this.btn_voltar.TabIndex = 5;
+            this.btn_voltar.Text = "Voltar";
+            this.btn_voltar.UseVisualStyleBackColor = true;
+            this.btn_voltar.Click += new System.EventHandler(this.btn_voltar_Click);
+            // 
+            // btn_limpar
+            // 
+            this.btn_limpar.Location = new System.Drawing.Point(375, 105);
+            this.btn_limpar.Name = "btn_limpar";
+            this.btn_limpar.Size = new System.Drawing.Size(95, 30);
+            this.btn_limpar.TabIndex = 6;
+            this.btn_limpar.Text = "Limpar";
+            this.btn_limpar.UseVisualStyleBackColor = true;
+            this.btn_limpar.Click += new System.EventHandler(this.btn_limpar_Click);
+            // 
+            // btn_sair
+            // 
+            this.btn_sair.Location = new System.Drawing.Point(646, 59);
+            this.btn_sair.Name = "btn_sair";
+            this.btn_sair.Size = new System.Drawing.Size(75, 23);
+            this.btn_sair.TabIndex = 7;
+            this.btn_sair.Text = "Sair";
+            this.btn_sair.UseVisualStyleBackColor = true;
+            this.btn_sair.Click += new System.EventHandler(this.btn_sair_Click);
+            // 
+            // txt_nome
+            // 
+            this.txt_nome.AutoSize = true;
+            this.txt_nome.Location = new System.Drawing.Point(38, 419);
+            this.txt_nome.Name = "txt_nome";
+            this.txt_nome.Size = new System.Drawing.Size(43, 13);
+            this.txt_nome.TabIndex = 8;
+            this.txt_nome.Text = "Usuário";
+            this.txt_nome.Visible = false;
+            // 
+            // FormExcluirAluno
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt_nome);
+            this.Controls.Add(this.btn_sair);
+            this.Controls.Add(this.btn_limpar);
+            this.Controls.Add(this.btn_voltar);
+            this.Controls.Add(this.btn_excluir);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_id);
+            this.Controls.Add(this.dataGridView_alunos);
+            this.Name = "FormExcluirAluno";
+            this.Text = "Excluir Aluno";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_alunos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView_alunos;
+        private System.Windows.Forms.TextBox txt_id;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_excluir;
+        private System.Windows.Forms.Button btn_voltar;
+        private System.Windows.Forms.Button btn_limpar;
+        private System.Windows.Forms.Button btn_sair;
+        public System.Windows.Forms.Label txt_nome;
+    }
+}
---
+++ Alunos/FormExcluirAluno.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Alunos
+{
+    public partial class FormExcluirAluno : Form
+    {
+        private Database db;
+        public FormExcluirAluno()
+        {
+            InitializeComponent();
+            db = new Database();
+            DataTable alunos = db.BuscarAlunos();
+
+            dataGridView_alunos.DataSource = alunos;
+        }
+
+        private void btn_excluir_Click(object sender, EventArgs e)
+        {
+            String idAluno = txt_id.Text;
+
+            if(idAluno!=null)
+            {
+                bool alunoExcluido = db.ExcluirAluno(idAluno);
+                
+                if(alunoExcluido)
+                {
+                    MessageBox.Show("Aluno excluído com sucesso!");
+                    DataTable alunos = db.BuscarAlunos();
+                    dataGridView_alunos.DataSource = alunos;
+                }
+                else
+                {
+                    MessageBox.Show("Erro ao excluir aluno!");
+                }
+            } else
+            {
+                MessageBox.Show("Informe o ID do aluno a ser excluído");
+            }
+        }
+
+        private void btn_limpar_Click(object sender, EventArgs e)
+        {
+            txt_id.Clear();
+        }
+
+        private void btn_voltar_Click(object sender, EventArgs e)
+        {
+            FormTelaInicial telaInicial = new FormTelaInicial();
+            telaInicial.txt_nome.Text = txt_nome.Text;
+            this.Hide();
+            telaInicial.ShowDialog();
+            this.Close();
+        }
+
+        private void btn_sair_Click(object sender, EventArgs e)
+        {
+            FormLogin telaLogin = new FormLogin();
+            this.Hide();
+            telaLogin.ShowDialog();
+            this.Close();
+        }
+    }
+}
